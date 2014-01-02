95f3b576e8cf17ef4c69e5a2c31ac556924462e0
M. Liebmann <tuxcode.bbg@gmail.com>
2014-01-02 12:30:03 +0100

    [apollo] Update /etc/init.d/S10_stb_sys_startup.sh

--- skel-root/apollo/etc/init.d/S10_stb_sys_startup.sh
+++ skel-root/apollo/etc/init.d/S10_stb_sys_startup.sh
@@ -2,7 +2,15 @@
 #set -x
 
 PATH=/var/bin:/bin:/var/sbin:/sbin
-KRNL_PATH=/lib/modules/$(uname -r)
+
+KRNL_VER=$(uname -r)
+echo "Using kernel $KRNL_VER"
+if [ "$KRNL_VER" = "2.6.34" }; then
+	OLD_KRNL=1
+else
+	OLD_KRNL=0
+fi
+KRNL_PATH=/lib/modules/$KRNL_VER
 
 dmesg -n1
 
@@ -13,28 +21,32 @@ im() {
 	if [ -f $KRNL_PATH/$1 ]; then
 		echo "insmod $*"
 		insmod $KRNL_PATH/$*
+	else
+		echo ">>>>> Error: modul $KRNL_PATH/$* not found."
 	fi
 }
 
 echo ""
+#im kernel/drivers/net/tun.ko
 im extra/lnxplatnativeDrv.ko
 im extra/lnxKKALDrv.ko
 im extra/lnxnotifyqDrv.ko
 im extra/lnxplatDrv.ko
-im extra/lnxplatSAR.ko
+[ "$OLD_KRNL" = "0" ] && im extra/lnxdebugDrv.ko
+[ "$OLD_KRNL" = "1" ] && im extra/lnxplatSAR.ko
 im extra/lnxscsDrv.ko
-im extra/lnxfssDrv.ko
+[ "$OLD_KRNL" = "1" ] && im extra/lnxfssDrv.ko
 im extra/lnxcssDrv.ko
 im extra/lnxtmasDrv.ko
 im extra/lnxtmvssDrvGPL.ko
 im extra/lnxtmvssDrv.ko
 im extra/lnxpvrDrv.ko
 im extra/lnxdvbciDrv.ko
-im extra/lnxIPfeDrv.ko
+[ "$OLD_KRNL" = "1" ] && im extra/lnxIPfeDrv.ko
 im extra/framebuffer.ko cnxtfb_standalone=1 cnxtfb_hdwidth=1280 cnxtfb_hdheight=720 cnxtfb_autoscale_sd=2
 
 if [ -f /opt/.load_3ddrivers ] ; then
-echo ""
+	echo ""
 	im extra/pvrsrvkm.ko
 	im extra/pvrnxpdc.ko
 	im extra/pvrvssbc.ko
@@ -50,7 +62,12 @@ dt -t"Loading drivers..."
 logoview --background --timeout=20 --logo=/var/share/icons/logo-bbg.jpg
 
 echo ""
-im dvb/dvb-core.ko
+
+if [ "$OLD_KRNL" = "1" ]; then
+	im dvb/dvb-core.ko
+else
+	im kernel/drivers/media/dvb/dvb-core/dvb-core.ko
+fi
 im extra/typhoon.ko
 im extra/a8296.ko
 im extra/av201x.ko
@@ -58,15 +75,25 @@ im extra/sharp780x.ko
 im extra/avl6211.ko
 im extra/dvb_api_prop.ko
 im extra/dvb_api.ko
-im fs/cifs/cifs.ko
+if [ "$OLD_KRNL" = "1" ]; then
+	im fs/cifs/cifs.ko
+else
+	im kernel/fs/cifs/cifs.ko
+fi
 
 if [ -e /var/etc/.load_wlan_drivers ] ; then
 	echo ""
-	im net/wireless/cfg80211.ko
-	im net/wireless/lib80211.ko
-	im net/wireless/8192cu/8192cu.ko
-	im net/wireless/8712u/8712u.ko
-
+	if [ "$OLD_KRNL" = "1"]; then
+		im net/wireless/cfg80211.ko
+		im net/wireless/lib80211.ko
+		im net/wireless/8192cu/8192cu.ko
+		im net/wireless/8712u/8712u.ko
+	else
+		im kernel/net/wireless/cfg80211.ko
+		im kernel/net/wireless/lib80211.ko
+		im extra/8192cu.ko
+		im extra/8712u.ko
+	fi
 #	im net/wireless/rt2870/rt2870sta.ko
 #	im net/wireless/rt2x00/rt2500usb.ko
 #	im net/wireless/rt2x00/rt2800lib.ko
