ae7be0ae9631dacafe34ae7d83a42b5a8220277a
M. Liebmann <tuxcode.bbg@gmail.com>
2014-01-26 15:30:34 +0100

    Add xupnpd target

--- make/extras1.mk
+++ make/extras1.mk
@@ -42,3 +42,75 @@ $(DEPDIR)/opkg: $(ARCHIVE)/opkg-$(OPKG_VER).tar.gz | $(TARGETPREFIX)
 	PKG_VER=$(OPKG_VER) $(OPKG_SH) $(CONTROL_DIR)/opkg
 	rm -rf $(PKGPREFIX)
 	touch $@
+
+XUPNP_DEFREF = r392
+XUPNP_DL_PATH = http://tsdemuxer.googlecode.com/svn/trunk/xupnpd
+
+$(SOURCE_DIR)/xupnp/src/Makefile:
+	if [ -e $(SOURCE_DIR)/xupnp ]; then \
+		rm -fr $(SOURCE_DIR)/xupnp; \
+	fi; \
+	mkdir -p $(SOURCE_DIR)/xupnp;
+	cd $(SOURCE_DIR)/xupnp; \
+		git svn init $(XUPNP_DL_PATH) --ignore-paths="^src/lua-5" ; \
+		git svn fetch --revision=360:HEAD; \
+		git svn rebase; \
+		ID=$$(git svn find-rev $(XUPNP_DEFREF)); \
+		git checkout $$ID; \
+		git checkout -b work; \
+		git am $(PATCHES)/xupnp/*.patch; \
+		git rebase master
+
+xupnpd-update: $(SOURCE_DIR)/xupnp/src/Makefile | $(TARGETPREFIX)
+	cd $(SOURCE_DIR)/xupnp; \
+		REV1=$$(git svn find-rev master --before HEAD); \
+		git checkout master; \
+		git svn fetch; \
+		git svn rebase; \
+		git checkout work; \
+		REV2=$$(git svn find-rev master --before HEAD); \
+		if [ ! "$$REV1" = "$$REV2" ]; then \
+			echo "before: r$$REV1, after: r$$REV2"; \
+			echo "git rebase master"; \
+			git rebase master; \
+		else \
+			echo "before: r$$REV1, after: r$$REV2"; \
+			echo "No changes..."; \
+		fi
+
+$(D)/xupnpd: $(SOURCE_DIR)/xupnp/src/Makefile | $(TARGETPREFIX)
+	set -e; cd $(SOURCE_DIR)/xupnp; \
+		git checkout work; \
+		cd src; \
+		make clean; \
+		make embedded \
+			CC=$(TARGET)-gcc \
+			STRIP=$(TARGET)-strip \
+			LUAFLAGS="-I$(TARGETPREFIX)/include -L$(TARGETPREFIX)/lib"; \
+	rm -rf $(PKGPREFIX)
+	mkdir -p $(PKGPREFIX)/bin
+	cp -a $(SOURCE_DIR)/xupnp/src/xupnpd $(PKGPREFIX)/bin
+	mkdir -p $(PKGPREFIX)/usr/share/xupnpd/playlists/example
+	cp -a $(SOURCE_DIR)/xupnp/src/playlists/example/* $(PKGPREFIX)/usr/share/xupnpd/playlists/example
+	cp -fd $(SOURCE_DIR)/xupnp/src/playlists/* $(PKGPREFIX)/usr/share/xupnpd/playlists/example > /dev/null 2>&1 || true
+	mkdir -p $(PKGPREFIX)/usr/share/xupnpd/plugins
+	cp -a $(SOURCE_DIR)/xupnp/src/plugins/* $(PKGPREFIX)/usr/share/xupnpd/plugins
+	mkdir -p $(PKGPREFIX)/usr/share/xupnpd/profiles
+	cp -a $(SOURCE_DIR)/xupnp/src/profiles/* $(PKGPREFIX)/usr/share/xupnpd/profiles
+	mkdir -p $(PKGPREFIX)/usr/share/xupnpd/ui
+	cp -a $(SOURCE_DIR)/xupnp/src/ui/* $(PKGPREFIX)/usr/share/xupnpd/ui
+	mkdir -p $(PKGPREFIX)/usr/share/xupnpd/www
+	cp -a $(SOURCE_DIR)/xupnp/src/www/* $(PKGPREFIX)/usr/share/xupnpd/www
+	cp -a $(SOURCE_DIR)/xupnp/src/*.lua  $(PKGPREFIX)/usr/share/xupnpd
+	install -m 0755 -D $(SCRIPTS)/xupnpd.init $(PKGPREFIX)/etc/init.d/xupnpd
+	ln -sf xupnpd $(PKGPREFIX)/etc/init.d/S80xupnpd
+	ln -sf xupnpd $(PKGPREFIX)/etc/init.d/K20xupnpd
+	cp -frd $(PKGPREFIX)/. $(TARGETPREFIX)
+	ln -s /usr/share $(PKGPREFIX)/share
+	cd $(SOURCE_DIR)/xupnp; \
+		XUPNP_SVN=$$(git svn find-rev master --before HEAD); \
+		PKG_VER=svnr$$XUPNP_SVN \
+			PKG_DEP=`opkg-find-requires.sh $(PKGPREFIX)` \
+				$(OPKG_SH) $(CONTROL_DIR)/xupnpd
+	rm -rf $(PKGPREFIX)
+	touch $@
