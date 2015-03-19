cf8b38d746a3d114dfec23584c264838c0a6db36
M. Liebmann <tuxcode.bbg@gmail.com>
2015-03-19 11:28:22 +0100

    Update build crosstool

--- make/crosstool.mk
+++ make/crosstool.mk
@@ -1,5 +1,16 @@
 # makefile to build crosstool
 
+## crosstool config ##############################################
+ifeq ($(PLATFORM), nevis)
+CT_NG_CONFIG = $(PATCHES)/ct-ng-1.20/ct-ng-nevis-1.20.0-2.config
+else
+ifeq ($(UCLIBC_BUILD), 1)
+CT_NG_CONFIG = $(PATCHES)/ct-ng-1.20/ct-ng-1.20.0-2.config
+else
+CT_NG_CONFIG = $(PATCHES)/ct-ng-1.20/ct-ng-1.20.0-1-glibc.config
+endif ## ifeq ($(UCLIBC_BUILD), 1)
+endif ## ifeq ($(PLATFORM), nevis)
+
 ## nevis crosstool ###############################################
 ifeq ($(PLATFORM), nevis)
 
@@ -12,11 +23,13 @@ crosstool: $(CROSS_DIR)/bin/$(TARGET)-gcc
 
 $(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz $(ARCHIVE)/linux-2.6.26.8.tar.bz2 $(ARCHIVE)/binutils-linaro-$(CT_NG_NEVIS_BINUTILS_VER).tar.bz2
 	make $(BUILD_TMP)
-	mkdir -p $(CROSS_DIR)
+	if [ ! -e $(CROSS_DIR) ]; then \
+		mkdir -p $(CROSS_DIR); \
+	fi;
 	$(REMOVE)/crosstool-ng-$(CROSSTOOL_NG_VER)
 	$(UNTAR)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz
 	set -e; unset CONFIG_SITE; cd $(BUILD_TMP)/crosstool-ng-$(CROSSTOOL_NG_VER); \
-		cp -a $(PATCHES)/ct-ng-1.20/ct-ng-nevis-$(CROSSTOOL_NG_VER)-1.config .config; \
+		cp -a $(CT_NG_CONFIG) .config; \
 		\
 		NUM_CPUS=$$(expr `getconf _NPROCESSORS_ONLN` \* 2); \
 		MEM_512M=$$(awk '/MemTotal/ {M=int($$2/1024/512); print M==0?1:M}' /proc/meminfo); \
@@ -29,31 +42,37 @@ $(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.
 			sed -i 's#svn://svn.eglibc.org#http://www.eglibc.org/svn#' \
 				scripts/build/libc/eglibc.sh || \
 			true; \
-		mkdir -p targets/src/; \
-		tar -C targets/src/ -xf $(ARCHIVE)/linux-2.6.26.8.tar.bz2; \
-		(cd targets/src/linux-2.6.26.8 && \
-			patch -p1 -i $(PATCHES)/linux-2.6.26.8-new-make.patch && \
-			patch -p1 -i $(PATCHES)/linux-2.6.26.8-rename-getline.patch); \
-		ln -sf linux-2.6.26.8 targets/src/linux-custom; \
-		touch targets/src/.linux-custom.extracted; \
 		export CST_BASE_DIR=$(BASE_DIR); \
+		export CST_CUSTOM_KERNEL=$(ARCHIVE)/cst-kernel_2.6.34.13-cnxt_2012-12-09_1613_6ff43b3.tar.xz; \
+		test -f ./configure || ./bootstrap && \
 		./configure --enable-local; MAKELEVEL=0 make; chmod 0755 ct-ng; \
 		./ct-ng oldconfig; \
 		./ct-ng build
 	ln -sf sys-root/lib $(CROSS_BASE)/$(TARGET)/
 
-## apollo / kronos crosstool ###############################################
+
+#		mkdir -p targets/src/; \
+#		tar -C targets/src/ -xf $(ARCHIVE)/linux-2.6.26.8.tar.bz2; \
+#		(cd targets/src/linux-2.6.26.8 && \
+#			patch -p1 -i $(PATCHES)/linux-2.6.26.8-new-make.patch && \
+#			patch -p1 -i $(PATCHES)/linux-2.6.26.8-rename-getline.patch); \
+#		ln -sf linux-2.6.26.8 targets/src/linux-custom; \
+#		touch targets/src/.linux-custom.extracted; \
+
 else ## ifeq ($(PLATFORM), nevis)
+## apollo / kronos crosstool ###############################################
 
 crosstool: $(CROSS_DIR)/bin/$(TARGET)-gcc
 
 $(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz
 	make $(BUILD_TMP)
-	mkdir -p $(CROSS_DIR)
+	if [ ! -e $(CROSS_DIR) ]; then \
+		mkdir -p $(CROSS_DIR); \
+	fi;
 	$(REMOVE)/crosstool-ng-$(CROSSTOOL_NG_VER)
 	$(UNTAR)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz
 	set -e; unset CONFIG_SITE; cd $(BUILD_TMP)/crosstool-ng-$(CROSSTOOL_NG_VER); \
-		cp -a $(PATCHES)/ct-ng-1.20/ct-ng-1.20-config-2-glibc .config; \
+		cp -a $(CT_NG_CONFIG) .config; \
 		\
 		NUM_CPUS=$$(expr `getconf _NPROCESSORS_ONLN` \* 2); \
 		MEM_512M=$$(awk '/MemTotal/ {M=int($$2/1024/512); print M==0?1:M}' /proc/meminfo); \
@@ -61,7 +80,16 @@ $(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.
 		test $$NUM_CPUS -lt 1 && NUM_CPUS=1; \
 		sed -i "s@^CT_PARALLEL_JOBS=.*@CT_PARALLEL_JOBS=$$NUM_CPUS@" .config; \
 		\
+		if [ "$(UCLIBC_BUILD)" = "1" ]; then \
+			cp $(PATCHES)/ct-ng-1.20/999-ppl-0_11_2-fix-configure-for-64bit-host.patch patches/ppl/0.11.2; \
+			cp $(PATCHES)/ct-ng-1.20/900-pull-socket_type-h-from-eglibc.patch patches/uClibc/0.9.33.2; \
+			cp $(PATCHES)/ct-ng-1.20/901-gettimeofday.c-use-the-same-type-as-in-header.patch patches/uClibc/0.9.33.2; \
+			export CST_UCLIBC_CONFIG="$(PATCHES)/ct-ng-1.20/ct-ng-uClibc-0.9.33.2.config"; \
+		fi; \
+		\
 		export CST_BASE_DIR=$(BASE_DIR); \
+		export CST_CUSTOM_KERNEL=$(ARCHIVE)/cst-kernel_cst_3.10_2015-03-13_1913_a32e261.tar.xz; \
+		test -f ./configure || ./bootstrap && \
 		./configure --enable-local; MAKELEVEL=0 make; chmod 0755 ct-ng; \
 		./ct-ng oldconfig; \
 		./ct-ng build
@@ -78,11 +106,8 @@ crossmenuconfig: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz
 	$(REMOVE)/crosstool-ng-$(CROSSTOOL_NG_VER)
 	$(UNTAR)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz
 	set -e; unset CONFIG_SITE; cd $(BUILD_TMP)/crosstool-ng-$(CROSSTOOL_NG_VER); \
-		if [ "$(PLATFORM)" = "nevis" ]; then \
-			cp -a $(PATCHES)/ct-ng-1.20/ct-ng-nevis-$(CROSSTOOL_NG_VER)-1.config .config; \
-		else \
-			cp -a $(PATCHES)/ct-ng-1.20/ct-ng-1.20-config-2-glibc .config; \
-		fi; \
+		cp -a $(CT_NG_CONFIG) .config; \
+		test -f ./configure || ./bootstrap && \
 		./configure --enable-local; MAKELEVEL=0 make; chmod 0755 ct-ng; \
 		./ct-ng menuconfig
 
