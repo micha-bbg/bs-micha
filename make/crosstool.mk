4a003be6c7e08428792be41a2c59aa4ba121e955
M. Liebmann <tuxcode.bbg@gmail.com>
2015-03-21 13:39:27 +0100

    Update make/crosstool.mk

--- make/crosstool.mk
+++ make/crosstool.mk
@@ -1,34 +1,14 @@
 # makefile to build crosstool
 
-## crosstool config ##############################################
-
-USE_UCLIBC_NG = 1
-
-ifeq ($(PLATFORM), nevis)
-CT_NG_CONFIG = $(PATCHES)/ct-ng-1.20/ct-ng-nevis-1.20.0-2.config
-else
-ifeq ($(UCLIBC_BUILD), 1)
-ifeq ($(USE_UCLIBC_NG), 1)
-CT_NG_CONFIG = $(PATCHES)/ct-ng-1.20/ct-ng-1.20.0-3.config
-else
-CT_NG_CONFIG = $(PATCHES)/ct-ng-1.20/ct-ng-1.20.0-2.config
-endif ## ($(USE_UCLIBC_NG), 1)
-else
-CT_NG_CONFIG = $(PATCHES)/ct-ng-1.20/ct-ng-1.20.0-1-glibc.config
-endif ## ifeq ($(UCLIBC_BUILD), 1)
-endif ## ifeq ($(PLATFORM), nevis)
-
 ## nevis crosstool ###############################################
 ifeq ($(PLATFORM), nevis)
 
-CT_NG_NEVIS_BINUTILS_VER = 2.23.2-2013.06
-
-$(ARCHIVE)/binutils-linaro-$(CT_NG_NEVIS_BINUTILS_VER).tar.bz2:
-	$(WGET) http://releases.linaro.org/13.06/components/toolchain/binutils-linaro/binutils-linaro-$(CT_NG_NEVIS_BINUTILS_VER).tar.bz2
+CT_NG_CONFIG  = $(PATCHES)/ct-ng-1.20/ct-ng-nevis-1.20.0-2.config
+CUSTOM_KERNEL = $(ARCHIVE)/cst-kernel_2.6.34.13-cnxt_2012-12-09_1613_6ff43b3.tar.xz
 
 crosstool: $(CROSS_DIR)/bin/$(TARGET)-gcc
 
-$(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz $(ARCHIVE)/linux-2.6.26.8.tar.bz2 $(ARCHIVE)/binutils-linaro-$(CT_NG_NEVIS_BINUTILS_VER).tar.bz2
+$(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz $(CUSTOM_KERNEL)
 	make $(BUILD_TMP)
 	if [ ! -e $(CROSS_DIR) ]; then \
 		mkdir -p $(CROSS_DIR); \
@@ -50,28 +30,34 @@ $(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.
 				scripts/build/libc/eglibc.sh || \
 			true; \
 		export CST_BASE_DIR=$(BASE_DIR); \
-		export CST_CUSTOM_KERNEL=$(ARCHIVE)/cst-kernel_2.6.34.13-cnxt_2012-12-09_1613_6ff43b3.tar.xz; \
+		export CST_CUSTOM_KERNEL=$(CUSTOM_KERNEL); \
 		test -f ./configure || ./bootstrap && \
 		./configure --enable-local; MAKELEVEL=0 make; chmod 0755 ct-ng; \
 		./ct-ng oldconfig; \
 		./ct-ng build
 	ln -sf sys-root/lib $(CROSS_BASE)/$(TARGET)/
 
-
-#		mkdir -p targets/src/; \
-#		tar -C targets/src/ -xf $(ARCHIVE)/linux-2.6.26.8.tar.bz2; \
-#		(cd targets/src/linux-2.6.26.8 && \
-#			patch -p1 -i $(PATCHES)/linux-2.6.26.8-new-make.patch && \
-#			patch -p1 -i $(PATCHES)/linux-2.6.26.8-rename-getline.patch); \
-#		ln -sf linux-2.6.26.8 targets/src/linux-custom; \
-#		touch targets/src/.linux-custom.extracted; \
-
 else ## ifeq ($(PLATFORM), nevis)
 ## apollo / kronos crosstool ###############################################
 
+ifeq ($(UCLIBC_BUILD), 1)
+ifeq ($(USE_UCLIBC_NG), 1)
+## build apollo/kronos uClibc-ng
+CT_NG_CONFIG = $(PATCHES)/ct-ng-1.20/ct-ng-1.20.0-3.config
+else
+## build apollo/kronos uClibc
+CT_NG_CONFIG = $(PATCHES)/ct-ng-1.20/ct-ng-1.20.0-2.config
+endif ## ($(USE_UCLIBC_NG), 1)
+else
+## build apollo/kronos eglibc
+CT_NG_CONFIG = $(PATCHES)/ct-ng-1.20/ct-ng-1.20.0-1-glibc.config
+endif ## ifeq ($(UCLIBC_BUILD), 1)
+
+CUSTOM_KERNEL = $(ARCHIVE)/cst-kernel_cst_3.10_2015-03-13_1913_a32e261.tar.xz
+
 crosstool: $(CROSS_DIR)/bin/$(TARGET)-gcc
 
-$(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz
+$(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz $(CUSTOM_KERNEL)
 	make $(BUILD_TMP)
 	if [ ! -e $(CROSS_DIR) ]; then \
 		mkdir -p $(CROSS_DIR); \
@@ -99,7 +85,7 @@ $(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.
 		fi; \
 		\
 		export CST_BASE_DIR=$(BASE_DIR); \
-		export CST_CUSTOM_KERNEL=$(ARCHIVE)/cst-kernel_cst_3.10_2015-03-13_1913_a32e261.tar.xz; \
+		export CST_CUSTOM_KERNEL=$(CUSTOM_KERNEL); \
 		if [ "$(USE_UCLIBC_NG)" = "1" ]; then \
 			export CST_CUSTOM_UCLIBC=$(ARCHIVE)/uClibc-ng-1.0.1.tar.xz; \
 		fi; \
