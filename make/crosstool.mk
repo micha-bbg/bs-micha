a7700418f0ec84cdab55a47dedfe320aa45cdf90
M. Liebmann <tuxcode.bbg@gmail.com>
2015-04-12 16:17:03 +0200

    Update crosstool for nevis

--- make/crosstool.mk
+++ make/crosstool.mk
@@ -4,7 +4,8 @@
 ifeq ($(PLATFORM), nevis)
 
 CT_NG_CONFIG  = $(PATCHES)/ct-ng-1.20/ct-ng-nevis-1.20.0-2.config
-CUSTOM_KERNEL = $(ARCHIVE)/cst-kernel_2.6.34.13-cnxt_2012-12-09_1613_6ff43b3.tar.xz
+#CUSTOM_KERNEL = $(ARCHIVE)/cst-kernel_2.6.34.13-cnxt_2012-12-09_1613_6ff43b3.tar.xz
+CUSTOM_KERNEL = $(ARCHIVE)/linux-2.6.26.8.tar.bz2
 
 crosstool: $(CROSS_DIR)/bin/$(TARGET)-gcc
 
@@ -16,6 +17,7 @@ $(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.
 	$(REMOVE)/crosstool-ng-$(CROSSTOOL_NG_VER)
 	$(UNTAR)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.xz
 	set -e; unset CONFIG_SITE; cd $(BUILD_TMP)/crosstool-ng-$(CROSSTOOL_NG_VER); \
+		cp $(PATCHES)/ct-ng-1.20/999-ppl-0_11_2-fix-configure-for-64bit-host.patch patches/ppl/0.11.2; \
 		cp -a $(CT_NG_CONFIG) .config; \
 		\
 		NUM_CPUS=$$(expr `getconf _NPROCESSORS_ONLN` \* 2); \
@@ -24,11 +26,14 @@ $(CROSS_DIR)/bin/$(TARGET)-gcc: $(ARCHIVE)/crosstool-ng-$(CROSSTOOL_NG_VER).tar.
 		test $$NUM_CPUS -lt 1 && NUM_CPUS=1; \
 		sed -i "s@^CT_PARALLEL_JOBS=.*@CT_PARALLEL_JOBS=$$NUM_CPUS@" .config; \
 		\
-		cp $(PATCHES)/ct-ng-1.20/999-ppl-0_11_2-fix-configure-for-64bit-host.patch patches/ppl/0.11.2; \
-		test "$(GIT_PROTOCOL)" = http && \
-			sed -i 's#svn://svn.eglibc.org#http://www.eglibc.org/svn#' \
-				scripts/build/libc/eglibc.sh || \
-			true; \
+		mkdir -p targets/src/; \
+		tar -C targets/src/ -xf $(CUSTOM_KERNEL); \
+		(cd targets/src/linux-2.6.26.8 && \
+			patch -p1 -i $(PATCHES)/kernel/linux-2.6.26.8-new-make.patch && \
+			patch -p1 -i $(PATCHES)/kernel/linux-2.6.26.8-rename-getline.patch); \
+		ln -sf linux-2.6.26.8 targets/src/linux-custom; \
+		touch targets/src/.linux-custom.extracted; \
+		\
 		export CST_BASE_DIR=$(BASE_DIR); \
 		export CST_CUSTOM_KERNEL=$(CUSTOM_KERNEL); \
 		test -f ./configure || ./bootstrap && \
