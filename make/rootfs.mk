463cf7db9ba639469ecbe00fb56a3912bfbe9d6e
M. Liebmann <tuxcode.bbg@gmail.com>
2015-11-19 13:41:33 +0100

    Update rootfs.mk

--- make/rootfs.mk
+++ make/rootfs.mk
@@ -2,7 +2,7 @@
 INSTALL = $(BUILD_TMP)/install
 INSTALL_TMP = $(INSTALL)/_TMP_
 
-ifeq ($(PLATFORM), apollo)
+ifneq ($(PLATFORM), nevis) # apollo/kronos
 
 $(D)/rootfs:
 	rm -rf $(INSTALL)
@@ -11,28 +11,33 @@ $(D)/rootfs:
 	cp -a $(BASE_DIR)/skel-root/common/* $(INSTALL)/
 	cp -a $(BASE_DIR)/skel-root/$(PLATFORM)/* $(INSTALL)/
 
-	cp -a $(TARGETPREFIX)/bin $(INSTALL)
-	cp -a $(TARGETPREFIX)/etc $(INSTALL)
-	cp -a $(TARGETPREFIX)/home $(INSTALL)
-	cp -a $(TARGETPREFIX)/lib $(INSTALL)
-	cp -a $(TARGETPREFIX)/mnt $(INSTALL)
-	cp -a $(TARGETPREFIX)/opt $(INSTALL)
-	cp -a $(TARGETPREFIX)/perf $(INSTALL)
-	cp -a $(TARGETPREFIX)/proc $(INSTALL)
-	cp -a $(TARGETPREFIX)/root $(INSTALL)
-	cp -a $(TARGETPREFIX)/sbin $(INSTALL)
-	cp -a $(TARGETPREFIX)/share $(INSTALL)
-	cp -a $(TARGETPREFIX)/usr $(INSTALL)
-	cp -a $(TARGETPREFIX)/var $(INSTALL)
-	cp -a $(TARGETPREFIX)/video_app $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/bin $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/etc $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/home $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/lib $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/mnt $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/opt $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/perf $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/proc $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/root $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/sbin $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/share $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/usr $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/var $(INSTALL)
+	cp -a $(TARGETPREFIX_BASE)/video_app $(INSTALL)
 
 	mkdir -p $(INSTALL)/dev $(INSTALL)/sys $(INSTALL)/tmp
 
 	ln -s bin/busybox $(INSTALL)/linuxrc
 #	ln -s usr/share $(INSTALL)/share
 
+	rm -fr $(INSTALL)/include
 	rm -fr $(INSTALL)/lib/engines
 	rm -fr $(INSTALL)/lib/pkgconfig
+
+	rm -fr $(INSTALL)/usr/include
+	rm -fr $(INSTALL)/usr/lib/engines
+	rm -fr $(INSTALL)/usr/lib/pkgconfig
 
 	## last neutrino pkg
 	mkdir -p $(INSTALL_TMP)/data
@@ -47,6 +52,8 @@ $(D)/rootfs:
 
 	find $(INSTALL) -name .gitignore -type f -print0 | xargs --no-run-if-empty -0 rm -f
 	find $(INSTALL)/lib \( -name '*.a' -o -name '*.la' \) -print0 | xargs --no-run-if-empty -0 rm -f
+	find $(INSTALL)/usr/lib \( -name '*.a' -o -name '*.la' \) -print0 | xargs --no-run-if-empty -0 rm -f
+	find $(INSTALL)/usr/lib/lua/5.2 \( -name '*.a' -o -name '*.la' \) -print0 | xargs --no-run-if-empty -0 rm -f
 	@echo ""
 	du -sh $(INSTALL)
 	@echo ""
